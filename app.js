93974c41756aeb6a4d1e98213df05478db50855f
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-04-10 22:52:35 -0600

    Add cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var routes = require('./routes/routes');
 require('./database/db');
 var favicon = require('serve-favicon');
 var setUpPassport = require('./utilities/setuppassport');
-var cors = require('cors');
+// var cors = require('cors');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,7 +25,7 @@ console.log('IP Address:', addr);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
